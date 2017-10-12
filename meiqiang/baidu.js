192ee8ddb0be429e2cce5edc1267bd4ac3e45d99
lyq <410869548@qq.com>
2017-10-12 14:08:40 +0800

    优化未登录时死循环的问题

--- meiqiang/baidu.js
+++ meiqiang/baidu.js
@@ -1,8 +1,12 @@
 chrome.extension.onRequest.addListener(
         function (request, sender, sendResponse) {
             if (request.action == "start") {
-                sendResponse({ end: "end" });
-                int = setInterval("buy()",1); 
+		sendResponse({ end: "end" });
+		if (document.getElementsByClassName('layout-user-downmenu-link')[0].innerHTML != '立即登录'){
+		        int = setInterval("buy()",1); 
+		}else{
+			alert('请先登录')
+		    }
             }
             if (request.action == "end") {
                 sendResponse({ start: "start" });
@@ -15,10 +19,10 @@ function buy(){
     var buy = document.getElementById('J_btnBuy')
     if (buy){
     if (buy.classList.contains('disabled')){
-        buy.innerHTML = '疯抢中...'
+	buy.innerHTML = '疯抢中...'
     }else{
-        buy.innerHTML = '订单提交中'
-        buy.click()
+	buy.innerHTML = '订单提交中'
+	buy.click()
     }
     }
 }
