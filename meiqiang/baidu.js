3e5ece5c58ecac17bc111053a8928d4d65d2835c
刘燕清 <410869548@qq.com>
2018-01-17 23:45:37 +0800

    兼容性重构

--- meiqiang/baidu.js
+++ meiqiang/baidu.js
@@ -1,28 +1,44 @@
-chrome.extension.onRequest.addListener(
-        function (request, sender, sendResponse) {
-            if (request.action == "start") {
-		sendResponse({ end: "end" });
-		if (document.getElementsByClassName('layout-user-downmenu-link')[0].innerHTML != '立即登录'){
-		        int = setInterval("buy()",1); 
-		}else{
-			alert('请先登录')
-		    }
-            }
-            if (request.action == "end") {
-                sendResponse({ start: "start" });
-                document.getElementById('J_btnBuy').innerHTML = '等待开始'
-                clearInterval(int)
+// chrome.extension.onRequest.addListener(
+//         function (request, sender, sendResponse) {
+//             if (request.action == "start") {
+// 		sendResponse({ end: "end" });
+// 		if (document.getElementsByClassName('layout-user-downmenu-link')[0].innerHTML != '立即登录'){
+// 		        int = setInterval("buy()",1); 
+// 		}else{
+// 			alert('请先登录')
+// 		    }
+//             }
+//             if (request.action == "end") {
+//                 sendResponse({ start: "start" });
+//                 document.getElementById('J_btnBuy').innerHTML = '等待开始'
+//                 clearInterval(int)
+//             }
+//         }
+// );
+
+browser.runtime.onMessage.addListener(request => {
+    function buy() {
+        var buy = document.getElementById('J_btnBuy')
+        if (buy) {
+            if (buy.classList.contains('disabled')) {
+                buy.innerHTML = '疯抢中...'
+            } else {
+                buy.innerHTML = '订单提交中'
+                buy.click()
             }
         }
-);
-function buy(){
-    var buy = document.getElementById('J_btnBuy')
-    if (buy){
-    if (buy.classList.contains('disabled')){
-	buy.innerHTML = '疯抢中...'
-    }else{
-	buy.innerHTML = '订单提交中'
-	buy.click()
     }
+    if (request.action == "start") {
+        if (document.getElementsByClassName('layout-user-downmenu-link')[0].innerHTML != '立即登录') {
+            int = setInterval(buy(), 1);
+        } else {
+            alert('请先登录')
+        }
+        return Promise.resolve({ action: "end" });
+    } else if (request.action == "end") {
+        document.getElementById('J_btnBuy').innerHTML = '等待开始'
+        clearInterval(int);
+        return Promise.resolve({ action: "start" });
     }
-}
+    
+});
--- meiqiang/popup.js
+++ meiqiang/popup.js
@@ -1,24 +1,73 @@
-$(document).on("click","#start",function(){
-    chrome.tabs.getSelected(function(tabs){
-        chrome.tabs.sendRequest(tabs.id,{action:"start"},function(response){
-            if(tabs.url.indexOf('detail.meizu.com') > 0){
-            if (response.end == "end"){
-                $("#start").css("display",'none')
-                $("#end").css("display",'initial')
-            }
-            }else{
+$(document).on("click", "#start", function () {
+    function logTabs(tabs) {
+        for (let tab of tabs) {
+            // tab.url requires the `tabs` permission
+            if (tab.url.indexOf('detail.meizu.com') > 0) {
+                browser.tabs.sendMessage(
+                    tab.id,
+                    { action: "start" }
+                ).then(response => {
+                    if (response.action == "end") {
+                        $("#start").css("display", 'none')
+                        $("#end").css("display", 'initial')
+                    }
+                }).catch(onError);
+            } else {
                 alert("请进去购买详情页使用")
             }
-        })
-    })
+        }
+    }
+
+    function onError(error) {
+        console.log(`Error: ${error}`);
+    }
+
+    var querying = browser.tabs.query({ currentWindow: true, active: true });
+    querying.then(logTabs, onError);
+    // chrome.tabs.getSelected(function(tabs){
+    //     chrome.tabs.sendRequest(tabs.id,{action:"start"},function(response){
+    //         if(tabs.url.indexOf('detail.meizu.com') > 0){
+    //         if (response.end == "end"){
+    //             $("#start").css("display",'none')
+    //             $("#end").css("display",'initial')
+    //         }
+    //         }else{
+    //             alert("请进去购买详情页使用")
+    //         }
+    //     })
+    // })
 })
-$(document).on("click","#end",function(){
-    chrome.tabs.getSelected(function(tabs){
-        chrome.tabs.sendRequest(tabs.id,{action:"end"},function(response){
-            if (response.start == "start"){
-                $("#end").css("display",'none')
-                $("#start").css("display",'initial')
+$(document).on("click", "#end", function () {
+    var querying = browser.tabs.query({ currentWindow: true, active: true });
+    querying.then(logTabs, onError);
+    function logTabs(tabs) {
+        for (let tab of tabs) {
+            // tab.url requires the `tabs` permission
+            if (tab.url.indexOf('detail.meizu.com') > 0) {
+                browser.tabs.sendMessage(
+                    tab.id,
+                    { action: "end" }
+                ).then(response => {
+                    if (response.action == "start") {
+                        $("#end").css("display", 'none')
+                        $("#start").css("display", 'initial')
+                    }
+                }).catch(onError);
+            } else {
+                alert("请进去购买详情页使用")
             }
-        })
-    })
+        }
+    }
+
+    function onError(error) {
+        (`Error: ${error}`);
+    }
+    // chrome.tabs.getSelected(function (tabs) {
+    //     chrome.tabs.sendRequest(tabs.id, { action: "end" }, function (response) {
+    //         if (response.start == "start") {
+    //             $("#end").css("display", 'none')
+    //             $("#start").css("display", 'initial')
+    //         }
+    //     })
+    // })
 })
